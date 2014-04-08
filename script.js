221c1d4966a84512d17b280b6c8f5ff732b382e7
Lingqiang Kong <klq@fake.com>
2014-04-08 23:07:39 +0000

    handle commas, whitespaces in text inputs

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ function Game (n) {
         this.feedback = [];
         this.win = false;
 
-
         while (this.answer.length < this.n_digits) {
             rand = Math.floor(Math.random() * 10);  //random digits from 0-9 
-            console.log(rand);
-            console.log($.inArray(rand, this.answer));
-            if ($.inArray(rand, this.answer) < 0) {
+            if ($.inArray(rand, this.answer) < 0) { // no repeating digits
                 this.answer.push(rand);
             }
         }
@@ -40,7 +37,7 @@ function Game (n) {
     this.feedback.push([place_hits, digit_hits]); 
     };
 
-    this.validate = function(guess) {
+    this.validateGuess = function(guess) {
         // length = n_digits and consists of only digits
         return (guess.length == this.n_digits && /^\d+$/.test(guess));
 
@@ -59,7 +56,7 @@ function Game (n) {
             return parseInt(item, 10);
         });
     }
-}
+} // end of Game()
 
 var currGame = new Game(4);
 
@@ -73,37 +70,39 @@ $(document).ready(function() {
     $('#submitbutton').click(function() {
         
         //got text from submit
-        var guess = $("#guesstext").val();
-        if (!currGame.validate(guess)) {
-            alert("Go to hell!");
+        //replace any non-Digit characters (commas, white spaces etc) with empty string
+        var guess = $("#guesstext").val().replace(/[\D]+/g, '');
+        $("#guesstext").val('');
+
+        if (!currGame.validateGuess(guess)) {
+            alert("That is not a valid guess. Try again!");
         }
         else {
-            //make guess an array
+            //submit the guess in array form and get feedback
             var lastfb = currGame.submitGuess(currGame.toArrayGuess(guess));
-            
             //append last feedback
-            $("#hist").append("Last guess: " + guess + "----" + lastfb[0] + " As " + " and " + lastfb[1] + " Bs correctly. <br/>");
-            //get feedback
-            //display something
-            //..test if win already.
+            $("#hist").append("Guess #" + (currGame.n_guesses) + ": " + guess);
+            $("#hist").append("----" + lastfb[0] + " As " + " and " + lastfb[1] + " Bs correctly. <br/>");
+
+            //test if win already.
+            if (lastfb[0] === currGame.n_digits) {
+                this.win = true;
+                $("#hist").append("Congratulations! You win! <br/>");
+                //TODO: save this game record somewhere
+            }
+            else {
+                //check if used up all guesses
+                if (currGame.n_guesses === currGame.maxtry) {
+                    alert("Game Over! You lost.");
+                }
+            }
+            
         }
     });
 });
 
-
-/*
-$(document).ready(function() {
-     var testArray = ["test1","test2","test3","test4"];
-        var vPool="";
-        jQuery.each(testArray, function(i, val) {
-          vPool += val + "<br /> is the best <br />";
-        });
-
-       //We add vPool HTML content to #myDIV
-       $('#myDIV').html(vPool);
-        vPool=""; //clear vPool for next round in the loop 
-});
-*/
+// TODO
+//Add some session logs to keep track of the win-lose records of a user
 
 
 
